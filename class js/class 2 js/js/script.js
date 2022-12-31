ce0bd10e4ba610afa74aa661cd6a5e6aadaa4432
Shoaib Ghulam <shoaibghulam@gmail.com>
2022-12-31 19:37:23 +0500

    new changes

---
+++ class js/class 2 js/js/script.js
@@ -0,0 +1,68 @@
+// var excel=parseInt(prompt("Enter Excel Marks"));
+// var word=parseInt(prompt("Enter word Marks"));
+// var access=parseInt(prompt("Enter Access Marks"));
+// var totalMarks=300;
+// var obtainMarks=excel+word+access;
+// var pre=obtainMarks*100/totalMarks;
+// var Grade='';
+// if(pre>100){
+//     Grade="Mars Error"
+// }
+// else if(pre>=90){
+//     Grade='A'
+// }
+// else if(pre>=80){
+//     Grade='B'
+// }
+// else if(pre>=70){
+//     Grade='C'
+// }
+// else if(pre>=60){
+//     Grade='D'
+// }
+// else{
+//     Grade='F'
+// }
+// console.log(`Word=${word}`);
+// console.log(`Excel=${excel}`);
+// console.log(`Access=${access}`);
+// console.log(`Total Marks=${totalMarks}`);
+// console.log(`Obtain Marks=${obtainMarks}`);
+// console.log(`Precentage=${pre}%`);
+// console.log(`Grade=${Grade}`);
+// amount=3000
+// pincode=parseInt(prompt("Enter your Atm Pin Code"))
+// if(pincode==1234){
+//     var x=parseInt(prompt("press 1 for cash widthral  \n press 2 for check balance press 3 for cancel"))
+//     if(x==1){
+//      var cash=parseInt(prompt("Enter Amount"))
+//      if(cash<=amount){
+//         amount=amount-cash
+//         console.log(' Remaining amount is'+amount)
+//      }
+//      else{
+//         console.log("Invilid Balance")
+//      }
+//     }
+//     else if(x==2){
+//         console.log(`Your Remaning Amount is ${amount}`)
+//     }
+//     else if(x==3){
+//         console.log(`Thank you `)
+//     }
+//     else{
+//         console.log("invilid Instruction")
+//     }
+// }
+// else{
+//     console.log("invilid pin code")
+// }
+
+// username=prompt("enter user name");
+// pwd=prompt("enter password");
+// if(username=="shoaib" && (pwd=="12345" || pwd=="125")){
+//     console.log("Welcome Mr."+username)
+// }
+// else{
+//     console.log("Invilid username and password")
+// }
