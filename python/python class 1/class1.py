9278d1774a5d918360bffcec6167f0dbb1510f09
Shoaib Ghulam <shoaibghulam@gmail.com>
2023-01-01 19:38:42 +0500

    python calss 1

---
+++ python/python class 1/class1.py
@@ -0,0 +1,65 @@
+# # print("Hello world")
+# x=3  # integer type 
+# name="shoaib" #String
+# y=3.3 # float
+# asLogin=True 
+# print("the value of x is ",x)
+# print(x,y)
+# a=3
+# b=5
+# c=a+b
+# print(c)
+# x='3'
+# y='5'
+# a=33
+# b=float(a)
+# z=int(x)+int(y)
+# print(z)
+# print(b)
+# print(str(b))
+# print(type(x))
+# x=True
+# v=str(x)
+# print(v)
+# z=""
+# y=bool(z)
+# print(y)
+# about="hello my name is shoaib ghulam from turbat"
+# print(len(about))
+# x=2
+# print(x**3)
+
+# z=5
+# print(z%5)
+
+# a=5
+# a=a+2
+# a+=5  # a=a+5
+# print(a)
+# firstName="Shoaib"
+# lastName="Ghulam"
+# # fullname=firstName+" "+lastName
+# fullname=f"{firstName} {lastName}"
+# print(fullname)
+# print("Hello "+"world")
+# x=int(input("Enter X value "))
+# y=int(input("Enter Y value "))
+# c=x+y
+# print(c)
+# a=5
+# if a > 5:
+#     print("It is True")
+# else:
+#     print("it is False")
+
+pre=80
+grade=""
+if pre> 90:
+    grade="A"
+elif pre>80:
+    grade="B"
+elif pre > 70:
+    grade="C"
+else:
+    grade="fail"
+print(grade)
